77cbc3661e07f0cb2ff21633272bd53a71cd6623
AmirhosseinSamaee <amirhosseinir1688@gmail.com>
2021-10-12 10:17:03 +0330

    Initial commit

---
+++ src/api/api.js
@@ -0,0 +1,26 @@
+import Axios from 'axios'
+
+export const getAxiosInstanceJsonServer = () => {
+  return Axios.create({
+    baseURL: "http://localhost:3000/",
+    headers: {
+      API_KEY: "lsdkljfalksfjasdfkjlasfjklasdkfjsadjf"
+    }
+  });
+};
+export const getAxiosInstanceAuth = () => {
+  return Axios.create({
+    baseURL: "http://localhost:3000/api/",
+    headers: {
+      // API_KEY: "lsdkljfalksfjasdfkjlasfjklasdkfjsadjf"
+    }
+  });
+};
+export const getAxiosInstanceApi = () => {
+  return Axios.create({
+    baseURL: "http://localhost:3000/api/",
+    headers: {
+      'x-auth-token': localStorage.getItem("x-auth-token")
+    }
+  });
+};
